31538299404ee5cf59624ad19b86c89b97f76eff
git@github.com:vinuptomy <vinuptomy@outlook.com>
2020-02-15 00:33:39 +0100

    New components added

--- src/app/country/country.module.ts
+++ src/app/country/country.module.ts
@@ -1,13 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { CountryListComponent } from './country-list/country-list.component';
+import { CountryDetailComponent } from './country-detail/country-detail.component';
 
 
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    CountryListComponent, 
+    CountryDetailComponent
+  ],
   imports: [
     CommonModule,
     RouterModule
